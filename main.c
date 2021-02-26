6d930ce0de439eef40d0d8d23b3db0dcd3159b45
Daniel Ferreira de Barros Junior <danielf@uni9.pro.br>
2021-02-26 00:16:48 +0000

    Revisão - Comandos basico

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
+// Revisão 01
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+
+	// int, float, double, char, char[10]
+	int a, b;
+	float x, y;
+	double valor;
+	char letra;
+	char nome[50];
+	/*
+		+ - * / %
+		> >= < <= == !=
+		&& || ^
+		x++, x--, ++x, --x
+	 
+		if()
+		if() else()
+		if() else( if() )
+		switch()
+
+		while(?){}
+		do{} while(?)
+		for(???){}
+
+		int valor[10] (0-9)
+		int x[L][C]
+
+		char nome[100] // #include <string.h>
+
+		int nome_funcao(int x){
+			return ??;
+		}
+
+		void nome_procedimento(int x, int y){
+
+		}
+
+		Variavel Local e Global
+
+		Ponteiros (variavel que armazena endereço de memoria)
+
+		struct
+	*/
+	return 0;
 }
