e8a2c0610b5d818918ba8ac44fd8489580ef8562
Daniel Ferreira de Barros Junior <danielf@uni9.pro.br>
2021-02-26 01:10:45 +0000

    Exemplo Cronometro 01

--- main.c
+++ main.c
@@ -1,47 +1,37 @@
 #include <stdio.h>
-
-// Revisão 01
+#include <unistd.h>
+#include <stdlib.h>
+// Revisão 01.1
 int main(void) {
-
-	// int, float, double, char, char[10]
-	int a, b;
-	float x, y;
-	double valor;
-	char letra;
-	char nome[50];
-	/*
-		+ - * / %
-		> >= < <= == !=
-		&& || ^
-		x++, x--, ++x, --x
-	 
-		if()
-		if() else()
-		if() else( if() )
-		switch()
-
-		while(?){}
-		do{} while(?)
-		for(???){}
-
-		int valor[10] (0-9)
-		int x[L][C]
-
-		char nome[100] // #include <string.h>
-
-		int nome_funcao(int x){
-			return ??;
-		}
-
-		void nome_procedimento(int x, int y){
-
-		}
-
-		Variavel Local e Global
-
-		Ponteiros (variavel que armazena endereço de memoria)
-
-		struct
+	/*	
+		CRONOMETRO 01  ( sleep(1); )
+	 	Quantos minutos deseja contar: 2
+	 	00:00, 00:01, 00:02, 02:00 (2 minutos)
 	*/
+
+	int total=0, ciclos=0, seg = 00, min=0;
+
+
+	printf(" Quantos minutos deseja contar: ");
+	scanf("%d", &total);
+	total = total * 60;
+
+	while( ciclos <= total){
+	
+		printf("%02d:%02d\n", min, seg);
+		seg++;
+
+		if(seg % 60 == 0){
+			min++;
+			seg = 0;
+		}
+
+		sleep(1);
+		ciclos++;
+		system("clear");
+	}
+
+	printf("Tempo encerrado!!\n");
 	return 0;
+
 }
